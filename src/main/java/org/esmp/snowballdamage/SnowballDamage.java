3fb98532e14505d4e64d231e023762e2f163d7a3
Luke Schweickart <lukeschweickart@gmail.com>
2020-12-16 23:47:21 -0600

    Initial commit

---
+++ src/main/java/org/esmp/snowballdamage/SnowballDamage.java
@@ -0,0 +1,17 @@
+package org.esmp.snowballdamage;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SnowballDamage extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
