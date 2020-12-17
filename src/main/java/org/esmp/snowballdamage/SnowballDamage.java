ee9a5a3138090ce059b33896cb3eaad576c7f24a
Luke Schweickart <lukeschweickart@gmail.com>
2020-12-17 10:48:40 -0600

    Imports and startup/shutdown logic added

--- src/main/java/org/esmp/snowballdamage/SnowballDamage.java
+++ src/main/java/org/esmp/snowballdamage/SnowballDamage.java
@@ -1,17 +1,39 @@
 package org.esmp.snowballdamage;
 
+import java.io.File;
+
+import jdk.internal.instrumentation.Logger;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
+import org.bukkit.Effect;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class SnowballDamage extends JavaPlugin {
+public final class SnowballDamage extends JavaPlugin implements Listener {
+
+    private Logger logger;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "SnowballDamage Version " + getDescription().getVersion() + ChatColor.GREEN + " enabled!");
+        this.logger.info("Snowball Damage Enabled");
+        if (!(new File(getDataFolder(), "config.yml")).exists()) {
+            saveDefaultConfig();
+            getConfig().set("Snowballs.Damage", 4.0D);
+            saveConfig();
+        }
+        getServer().getPluginManager().registerEvents(this, (Plugin)this);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "SnowballDamage Version " + getDescription().getVersion() + ChatColor.GREEN + " disabled!");
+        this.logger.info("Snowball Damage Disabled");
     }
 }
